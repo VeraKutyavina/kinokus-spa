e69e22b385663157215b4acfff79a455c42675c9
VeraKutyavina <vera.kutiavina@stackers.team>
2024-01-04 21:20:03 +0300

    init next js

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+const TSComponent = () => {
+  return (
+    <div className="App">
+      <br />
+      <a href="/WithoutTS">Hello world page!</a>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default TSComponent;
--- src/styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.span`
-  display: block;
-
-  font-size: 1rem;
-  line-height: 1.25rem;
-`;
