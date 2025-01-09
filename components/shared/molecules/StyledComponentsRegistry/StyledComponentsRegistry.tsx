f635598b29a3a506eac70f7db5421ea3b878612c
VeraKutyavina <vera.kutiavina@gmail.com>
2025-01-09 14:53:29 +0300

    add register

---
+++ components/shared/molecules/StyledComponentsRegistry/StyledComponentsRegistry.tsx
@@ -0,0 +1,19 @@
+import React, { FC, PropsWithChildren, useState } from 'react';
+import { useServerInsertedHTML } from 'next/navigation';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+
+export const StyledComponentsRegistry: FC<PropsWithChildren> = ({ children }) => {
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== 'undefined') return <>{children}</>;
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>
+  );
+};
--- components/shared/organisms/ClientLogicLayout/ClientLogicLayout.tsx
+++ components/shared/organisms/ClientLogicLayout/ClientLogicLayout.tsx
@@ -4,12 +4,16 @@ import { FC, PropsWithChildren, useState } from 'react';
 import { ThemeContext } from 'components/contexts/ThemeContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import { StyledComponentsRegistry } from 'components/shared/molecules/StyledComponentsRegistry';
+
 export const ClientLogicLayout: FC<PropsWithChildren> = ({ children }) => {
   const [client] = useState(() => new QueryClient());
 
   return (
-    <ThemeContext>
-      <QueryClientProvider client={client}>{children}</QueryClientProvider>
-    </ThemeContext>
+    <StyledComponentsRegistry>
+      <ThemeContext>
+        <QueryClientProvider client={client}>{children}</QueryClientProvider>
+      </ThemeContext>
+    </StyledComponentsRegistry>
   );
 };
